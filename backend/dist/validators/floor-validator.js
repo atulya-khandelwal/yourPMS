c8d96a786f78e399c4a46b564514dc044eb43ad3
Atulya Khandelwal <atulyakhandelwal8691@gmail.com>
2025-05-09 15:40:48 +0530

    Remove dist folder from repo and add to .gitignore

--- backend/dist/validators/floor-validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createFloorSchema = void 0;
-const zod_1 = require("zod");
-exports.createFloorSchema = zod_1.z.object({
-    property_id: zod_1.z.number().int().positive(),
-});
--- backend/dist/validators/property-validator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createPropertySchema = void 0;
-const zod_1 = require("zod");
-exports.createPropertySchema = zod_1.z.object({
-    user_id: zod_1.z.number().int().positive(),
-    name: zod_1.z.string().min(1, 'Name is required'),
-    address: zod_1.z.string(),
-});
